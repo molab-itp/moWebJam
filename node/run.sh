5faccf9f3afe079f5ed185dfa5823db1f706c3d5
John Henry Thompson <jht9629@nyu.edu>
2024-09-15 09:49:29 -0400

    Update run.sh

--- node/run.sh
+++ node/run.sh
@@ -6,6 +6,6 @@ cd ../node
 
 # https://poets.org/poem/let-america-be-america-again
 # npm run start -- --ddebug --width_trim 0.40 --full
-# npm run start -- --group s0 --full 0 --portrait 1.0 --screen 1 --root "https://poets.org/poem/let-america-be-america-again"
-npm run start -- --group s1 --full 0 --portrait 1.0 --screen 1 --root "https://poets.org/poem/let-america-be-america-again"
+npm run start -- --group s0 --full 0 --portrait 1.0 --screen 1 --root "https://poets.org/poem/let-america-be-america-again"
+# npm run start -- --group s1 --full 0 --portrait 1.0 --screen 1 --root "https://poets.org/poem/let-america-be-america-again"
 
