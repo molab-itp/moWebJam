47598a75b9299560307076244e32871e96621861
John Henry Thompson <jht9629@nyu.edu>
2024-11-04 11:12:29 -0500

    run

--- node/run-let-dev.sh
+++ node/run-let-dev.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-
 cd ${0%/*}
-
 cd ../node
 
 # https://poets.org/poem/let-america-be-america-again
 # npm run start -- --ddebug --width_trim 0.40 --full
-# npm run start -- --group s0 --room m0-america --app mo-america-be --full 0 --portrait 1.0 --screen 1 --root "https://poets.org/poem/let-america-be-america-again"
-npm run start -- --group s0 --full 0 --portrait 1.0 --screen 1 --root "https://poets.org/poem/let-america-be-america-again"
+npm run start -- --group s0 --room m0-america --app mo-america-be --full 0 --portrait 1.0 --screen 1 --root "https://poets.org/poem/let-america-be-america-again"
--- node/run-let.sh
+++ node/run-let.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-
 cd ${0%/*}
-
 cd ../node
 
 # https://poets.org/poem/let-america-be-america-again
 # npm run start -- --ddebug --width_trim 0.40 --full
-npm run start -- --group s1 --full 1 --portrait 1.0 --screen 1 --root "https://poets.org/poem/let-america-be-america-again"
+npm run start -- --group s1 --room m1-america --app mo-america-be --full 0 --portrait 1.0 --screen 1 --root "https://poets.org/poem/let-america-be-america-again"
