7e65c056ae775fe1cf9fb9634e5c8b1ba470add1
John Henry Thompson <jht9629@nyu.edu>
2024-11-04 10:55:06 -0500

    run-let-dev

---
+++ node/run-let.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd ${0%/*}
+
+cd ../node
+
+# https://poets.org/poem/let-america-be-america-again
+# npm run start -- --ddebug --width_trim 0.40 --full
+# npm run start -- --group s0 --room m0-america --app mo-america-be --full 0 --portrait 1.0 --screen 1 --root "https://poets.org/poem/let-america-be-america-again"
+npm run start -- --group s1 --full 0 --portrait 1.0 --screen 1 --root "https://poets.org/poem/let-america-be-america-again"
+
+# my.mo_app
+# 'mo-america-be'
