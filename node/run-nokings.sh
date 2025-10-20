5d3ddafc4f6f678ed0329ca8d5d12dee3e2a32ba
John Henry Thompson <78864877+jht9629-nyu@users.noreply.github.com>
2025-10-20 10:40:36 -0400

    Create run-nokings.sh

---
+++ node/run-nokings.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cd ${0%/*}
+
+cd ../node
+
+# URL="https://molab-itp.github.io/moSalon/src/vote-no-fascism/?v=56&scroll=1"
+URL="https://woke-mind.github.io/woke-words/words/?v=63"
+URL="https://molab-itp.github.io/moSalon/src/videoplayer/?playlist=Axuf_zTf4TU"
+
+# https://poets.org/poem/let-america-be-america-again
+# npm run start -- --ddebug --width_trim 0.40 --full
+# npm run start -- --group s0 --full 0 --portrait 1.0 --screen 1 --root "https://poets.org/poem/let-america-be-america-again"
+
+PERIOD=12:00:0
+# PERIOD=2:30:0
+# PERIOD=1:0
+npm run start -- --restart_period $PERIOD --full 1 --portrait 1.00 --screen 1 --root "$URL"
+
